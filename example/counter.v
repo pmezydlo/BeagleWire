205f3c8b09afcd703484dcf0a7cccd02caeee8c7
Patryk <mezydlo.p@gmail.com>
2017-05-31 16:36:51 +0200

    added pcf to example fixed blink led example
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- example/counter.v
+++ example/counter.v
@@ -1,7 +1,7 @@
 module top(input CLK_100M, output [3:0] LED);
 
 	reg [25:0] counter = 0;
-	always @(posedge ) counter <= counter + 1;
+	always @(posedge CLK_100M) counter <= counter + 1;
 
-	assign leds[0:3] = counter[22:25];
+	assign LED[0:3] = counter[22:25];
 endmodule
