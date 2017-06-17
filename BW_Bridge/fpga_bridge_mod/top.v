72f64f9c279ea08f38612c27c26922183e5a6843
Patryk <mezydlo.p@gmail.com>
2017-06-17 19:56:00 +0200

    fix inout problem concerning the gpmc data/address port
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- BW_Bridge/fpga_bridge_mod/top.v
+++ BW_Bridge/fpga_bridge_mod/top.v
@@ -1,16 +1,32 @@
-module top (input         CLK_100M, 
-            output [3:0]  LED,
-            inout  [15:0] GPMC_AD,
-            input         GPMC_ADVN,
-            input         GPMC_CSN1,
-            input         GPMC_WEIN,
-            input         GPMC_OEN,
-            input         GPMC_CLK,
-            input  [1:0]  BTN,
-            output [7:0]  PMOD1,
-            output [7:0]  PMOD2,
-            output [7:0]  PMOD3,
-            output [7:0]  PMOD4);
+module top (input         clk, 
+            output [3:0]  led,
+            inout         gpmc_ad0, 
+            input         gpmc_ad1,
+            inout         gpmc_ad2, 
+            inout         gpmc_ad3, 
+            inout         gpmc_ad4, 
+            inout         gpmc_ad5, 
+            inout         gpmc_ad6,
+            inout         gpmc_ad7,
+            inout         gpmc_ad8, 
+            inout         gpmc_ad0, 
+            inout         gpmc_ad9, 
+            inout         gpmc_ad10, 
+            inout         gpmc_ad11, 
+            inout         gpmc_ad12, 
+            inout         gpmc_ad13, 
+            inout         gpmc_ad14, 
+            inout         gpmc_ad15,
+            input         gpmc_advn,
+            input         gpmc_csn1,
+            input         gpmc_wein,
+            input         gpmc_oen,
+            input         gpmc_clk,
+            input  [1:0]  btn,
+            output [7:0]  pmod1,
+            output [7:0]  pmod2,
+            output [7:0]  pmod3,
+            output [7:0]  pmod4);
 
 parameter ADDR_WIDTH = 4;
 parameter DATA_WIDTH = 16;
@@ -18,32 +34,44 @@ parameter DATA_WIDTH = 16;
 reg [ADDR_WIDTH:0] addr;
 reg [DATA_WIDTH-1:0] mem [ADDR_WIDTH-1:0];
 reg [DATA_WIDTH-1:0] data_out;
+reg [DATA_WIDTH-1:0] data_in;
+
+reg data_out;
+reg data_in;
 
 initial begin
     addr <= 3'b000;
     data_out <= 16'b0000_0000_0000_0000;
 end
 
-assign GPMC_AD = (!GPMC_CSN1 && GPMC_ADVN && !GPMC_OEN && GPMC_WEIN) ? data_out : 16'bz;
+SB_IO # (
+    .PIN_TYPE(6'b1010_01),
+    .PULLUP(1'b 0)
+) gpmc_ad_io [15:0] (
+    .PACKAGE_PIN({gpmc_ad0, gpmc_ad1, gpmc_ad2, gpmc_ad3, gpmc_ad4, gpmc_ad5, gpmc_ad6, gpmc_ad7, gpmc_ad8, gpmc_ad9, gpmc_ad10, gpmc_ad11, gpmc_ad12, gpmc_ad13, gpmc_ad14, gpmc_ad15}),
+    .OUTPUT_ENABLE(!gpmc_csn1 && gpmc_advn && !gpmc_oen && gpmc_wein),
+    .D_OUT_0(data_out),
+    .D_IN_0(data_in)
+);
 
-always @ (negedge CLK_100M)
+always @ (negedge gpmc_clk)
 begin : GPMC_LATCH_ADDRESS   
-    if (!GPMC_CSN1 && !GPMC_ADVN && GPMC_WEIN && GPMC_OEN)
-        addr <= GPMC_AD[ADDR_WIDTH-1:0];
+    if (!gpmc_csn1 && !gpmc_advn && gpmc_wein && gpmc_oen)
+        addr <= gpmc_ad[ADDR_WIDTH-1:0];
 end
 
-always @ (negedge CLK_100M)
+always @ (negedge gpmc_clk)
 begin : GPMC_WRITE_DATA   
-    if (!GPMC_CSN1 && GPMC_ADVN && !GPMC_WEIN && GPMC_OEN)
-        mem[addr] <= GPMC_AD[DATA_WIDTH-1:0];
+    if (!gpmc_csn1 && gpmc_advn && !gpmc_wein && gpmc_oen)
+        mem[addr] <= data_in;
 end
 
-always @ (negedge GPMC_CLK)
+always @ (negedge gpmc_clk)
 begin : GPMC_READ_DATA   
-    if (!GPMC_CSN1 && GPMC_ADVN && !GPMC_WEIN && GPMC_OEN)
+    if (!gpmc_csn1 && gpmc_advn && !gpmc_wein && gpmc_oen)
         data_out <= mem[addr];
 end
 
-assign LED = mem[0][3:0];
+assign led = mem[0][3:0];
 
 endmodule
