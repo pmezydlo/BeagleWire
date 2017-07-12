008d99092535019738f97915f8c7aa5ef12d7ead
Patryk <mezydlo.p@gmail.com>
2017-07-12 19:30:24 +0200

    update travis
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- scripts/install_kernel.sh
+++ scripts/install_kernel.sh
@@ -2,14 +2,18 @@ export SOURCE_BRANCH="4.12"
 export SOURCE_VERSION="rc7"
 export SOURCE_PLATFORM="bone1"
 
+wget -c https://releases.linaro.org/components/toolchain/binaries/5.3-2016.02/arm-linux-gnueabihf/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf.tar.xz
+tar xf gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf.tar.xz
+export CC=$PWD/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+
+echo "Using:" 
+${CC}gcc --version
+
 wget https://github.com/RobertCNelson/linux-stable-rcn-ee/archive/v$SOURCE_BRANCH-$SOURCE_VERSION.tar.gz
 tar xf v$SOURCE_BRANCH-$SOURCE_VERSION.tar.gz
 
 cd ./linux-stable-rcn-ee-$SOURCE_BRANCH-$SOURCE_VERSION
 
-make -j3 mrproper ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- LOCALVERSION=-$SOURCE_VERSION-$SOURCE_PLATFORM
-
+make -j3 mrproper ARCH=arm CROSS_COMPILE=${CC}
 wget -c "http://rcn-ee.net/deb/jessie-armhf/v$SOURCE_BRANCH.0-$SOURCE_VERSION-$SOURCE_PLATFORM/defconfig" -O .config
-
-make -j3 modules ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- LOCALVERSION=-$SOURCE_VERSION-$SOURCE_PLATFORM 2>&1
-
+make -j3 modules ARCH=arm CROSS_COMPILE=${CC}
