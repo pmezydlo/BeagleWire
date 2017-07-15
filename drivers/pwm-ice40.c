a8acbcdb4f4d4fb6f37afdf6de17c6588a06e76b
Patryk <mezydlo.p@gmail.com>
2017-07-15 13:56:25 +0200

    add pwm futures: - enable and disable pwm signal - set polarity
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- drivers/pwm-ice40.c
+++ drivers/pwm-ice40.c
@@ -39,20 +39,54 @@ static inline struct ice40_pwm *to_ice40_pwm(struct pwm_chip *chip)
 
 static int ice40_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm)
 {
+	struct ice40_pwm *ice40_pwm = to_ice40_pwm(chip);
+	u16 setup_reg_cache;
+
+	setup_reg_cache = ice40_pwm_read_reg(ice40_pwm->base,
+					     ICE40_PWM_SETUP_REG);
+	setup_reg_cache |= ICE40_PWM_SETUP_REG_EN_BIT;
+	ice40_pwm_write_reg(ice40_pwm->base, ICE40_PWM_SETUP_REG,
+			    setup_reg_cache);
 
 	return 0;
 }
 
 static void ice40_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm)
 {
+	struct ice40_pwm *ice40_pwm = to_ice40_pwm(chip);
+	u16 setup_reg_cache;
 
-
+	setup_reg_cache = ice40_pwm_read_reg(ice40_pwm->base,
+					     ICE40_PWM_SETUP_REG);
+	setup_reg_cache &= ~ICE40_PWM_SETUP_REG_EN_BIT;
+	ice40_pwm_write_reg(ice40_pwm->base, ICE40_PWM_SETUP_REG,
+			    setup_reg_cache);
 }
 
 static int ice40_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 			    int duty_ns, int period_ns)
 {
+	struct ice40_pwm *ice40_pwm = to_ice40_pwm(chip);
 
+	return 0;
+}
+
+static int ice40_pwm_set_poleaity(struct pwm_chip *chip, struct pwm_device *pwm,
+				  enum pwm_polarity polarity)
+{
+	struct ice40_pwm *ice40_pwm = to_ice40_pwm(chip);
+	u16 setup_reg_cache;
+
+	setup_reg_cache = ice40_pwm_read_reg(ice40_pwm->base,
+					     ICE40_PWM_SETUP_REG);
+
+	if (polarity == PWM_POLARITY_INVERSED)
+		setup_reg_cache |= ICE40_PWM_SETUP_REG_POL_BIT;
+	else
+		setup_reg_cache &= ~ICE40_PWM_SETUP_REG_POL_BIT;
+
+	ice40_pwm_write_reg(ice40_pwm->base, ICE40_PWM_SETUP_REG,
+			    setup_reg_cache);
 	return 0;
 }
 
@@ -60,19 +94,43 @@ static const struct pwm_ops ice40_pwm_ops = {
 	.config = ice40_pwm_config,
 	.enable = ice40_pwm_enable,
 	.disable = ice40_pwm_disable,
+	.set_polarity = ice40_pwm_set_poleaity,
 	.owner = THIS_MODULE,
 };
 
 static int ice40_pwm_probe(struct platform_device *pdev)
 {
+	struct ice40_pwm *ice40_pwm;
+	struct resource *res;
+	int ret;
 
+	ice40_pwm = devm_kzalloc(&pdev->dev, sizeof(*ice40_pwm), GFP_KERNEL);
+	if (!ice40_pwm)
+		return -ENOMEM;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	ice40_pwm->base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(ice40_pwm->base))
+		return PTR_ERR(ice40_pwm->base);
+
+	ice40_pwm->chip.dev = &pdev->dev;
+	ice40_pwm->chip.ops = &ice40_pwm_ops;
+	ice40_pwm->chip.base = -1;
+	ice40_pwm->chip.npwm = 1;
+
+	ret = pwmchip_add(&ice40_pwm->chip);
+	if (ret < 0)
+		return ret;
+
+	platform_set_drvdata(pdev, ice40_pwm);
 	return 0;
 }
 
 static int ice40_pwm_remove(struct platform_device *pdev)
 {
+	struct ice40_pwm *ice40_pwm = platform_get_drvdata(pdev);
 
-	return 0;
+	return pwmchip_remove(&ice40_pwm->chip);
 }
 
 static const struct of_device_id ice40_pwm_of_match[] = {
@@ -95,5 +153,5 @@ module_platform_driver(ice40_pwm_driver);
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Patryk Mezydlo, <mezydlo.p@gmail.com>");
-MODULE_DESCRIPTION("Driver for SPI controller on the ice40 FPGA");
+MODULE_DESCRIPTION("Driver for PWM controller on the ice40 FPGA");
 MODULE_VERSION("1.0");
