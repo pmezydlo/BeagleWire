fb2e823c51ff0c332860feea00bf68d4c16be22a
Patryk <mezydlo.p@gmail.com>
2017-07-22 11:58:53 +0200

    add license
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- drivers/spi-ice40.c
+++ drivers/spi-ice40.c
@@ -1,11 +1,20 @@
 /*
- * ice40 SPI driver
+ * Driver for memory mapped SPI ip core on ice40 FPGA
  *
- * Copyright (c) 2017 Patryk Mezydlo <mezydlo.p@gmail.com
+ * Copyright (C) 2017 Patryk Mezydlo <mezydlo.p@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/kernel.h>
