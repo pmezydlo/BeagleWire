fb50e7520935a8508aa0c10ca1a71e9833cd86a9
Patryk <mezydlo.p@gmail.com>
2017-08-09 11:35:58 +0200

    refactoring bridge lib code
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- bridge_lib/bw_bridge.c
+++ bridge_lib/bw_bridge.c
@@ -1,59 +1,60 @@
 #include "bw_bridge.h"
 
-int bw_init() {
-	uint32_t mem_address = BW_BRIDGE_MEM_ADR;
-	uint32_t mem_size = BW_BRIDGE_MEM_SIZE;
-	uint32_t page_mask, page_size;
+int bw_init(struct bridge *br, uint32_t mem_address, uint32_t mem_size) {
+	uint32_t page_mask;
+	uint32_t page_size;
 
 	page_size = sysconf(_SC_PAGESIZE);
-	alloc_mem_size = (((mem_size / page_size) + 1) * page_size);
+	br->alloc_mem_size = (((mem_size / page_size) + 1) * page_size);
 	page_mask = (page_size - 1);
 
-	mem_dev = open("/dev/mem", O_RDWR | O_SYNC);
-	if (mem_dev < 0)
+	br->mem_dev = open("/dev/mem", O_RDWR | O_SYNC);
+	if (br->mem_dev < 0)
 		return -EPERM;
 
-	mem_pointer = mmap(NULL,
-		           alloc_mem_size,
-			   PROT_READ | PROT_WRITE,
-                           MAP_SHARED,
-                           mem_dev,
-                           (mem_address & ~page_mask));
+	br->mem_pointer = mmap(NULL,
+		               br->alloc_mem_size,
+			       PROT_READ | PROT_WRITE,
+                               MAP_SHARED,
+                               br->mem_dev,
+                               (mem_address & ~page_mask));
 
-	if(mem_pointer == MAP_FAILED) {
+	if(br->mem_pointer == MAP_FAILED) {
 	      return -ENOMEM;
 	}
 
-	virt_addr = (mem_pointer + (mem_address & page_mask));
+	br->virt_addr = (br->mem_pointer + (mem_address & page_mask));
 
 	return 0;
 }
 
-void bw_close() {
-	if (munmap(mem_pointer, alloc_mem_size) == -1)
+void bw_close(struct bridge *br) {
+	if (munmap(br->mem_pointer, br->alloc_mem_size) == -1)
 		perror("Error un-mmapping the file");
 
-	close(mem_dev);
+	close(br->mem_dev);
 }
 
-uint16_t bw_get_word(uint16_t reg_addr) {
-	return *(uint16_t *)(virt_addr + reg_addr);
+uint16_t bw_get_word(struct bridge *br, uint16_t reg_addr) {
+	return *(uint16_t *)(br->virt_addr + reg_addr);
 }
 
-void bw_set_word(uint16_t reg_addr, uint16_t word) {
-	*(uint16_t *)(virt_addr + reg_addr) = word;
+void bw_set_word(struct bridge *br, uint16_t reg_addr, uint16_t word) {
+	*(uint16_t *)(br->virt_addr + reg_addr) = word;
 }
 
-void bw_set_fpga_mem(uint16_t reg_addr, const void* source, size_t reg_num) {
+void bw_set_fpga_mem(struct bridge *br, uint16_t reg_addr, const void* source,
+		     size_t reg_num) {
 	unsigned int c;
 	uint16_t *usrc = (uint16_t *)source;
 	for (c = 0; c < reg_num; c++)
-		*(uint16_t *)(virt_addr + reg_addr + c*2) = usrc[c];
+		*(uint16_t *)(br->virt_addr + reg_addr + c*2) = usrc[c];
 }
 
-void bw_get_fpga_mem(uint16_t reg_addr, void* destination, size_t reg_num) {
+void bw_get_fpga_mem(struct bridge *br, uint16_t reg_addr, void* destination,
+		     size_t reg_num) {
 	unsigned int c;
 	uint16_t *udst = (uint16_t *)destination;
 	for (c = 0; c < reg_num; c++)
-		udst[c] = *(uint16_t *)(virt_addr + reg_addr + c*2);
+		udst[c] = *(uint16_t *)(br->virt_addr + reg_addr + c*2);
 }
--- bridge_lib/bw_bridge.h
+++ bridge_lib/bw_bridge.h
@@ -9,19 +9,20 @@
 #include <time.h>
 #include <errno.h>
 
-#define BW_BRIDGE_MEM_ADR 0x01000000
-#define BW_BRIDGE_MEM_SIZE 0x20000
+struct bridge {
+	void		*virt_addr;
+	int		mem_dev;
+	uint32_t	alloc_mem_size;
+	void		*mem_pointer;
+};
 
-void *virt_addr;
-int mem_dev;
-uint32_t alloc_mem_size;
-void *mem_pointer;
-
-int bw_init();
-void bw_close();
-uint16_t bw_get_word(uint16_t reg_addr);
-void bw_set_word(uint16_t reg_addr, uint16_t word);
-void bw_set_fpga_mem(uint16_t reg_addr, const void* source, size_t reg_num);
-void bw_get_fpga_mem(uint16_t reg_addr, void* destination, size_t reg_num);
+int bridge_init();
+void bridge_close();
+uint16_t get_word(struct bridge *br, uint16_t reg_addr);
+void set_word(struct bridge *br, uint16_t reg_addr, uint16_t word);
+void set_fpga_mem(struct bridge *br, uint16_t reg_addr, const void* source,
+		     size_t reg_num);
+void get_fpga_mem(struct bridge *br, uint16_t reg_addr, void* destination,
+		  size_t reg_num);
 
 #endif
