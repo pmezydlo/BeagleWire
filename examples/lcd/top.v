8d9a5cc75aeeaf54e0345943d72bb42f20ee9814
Patryk <mezydlo.p@gmail.com>
2017-11-28 22:28:21 +0100

    add support for rgb565
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- examples/lcd/top.v
+++ examples/lcd/top.v
@@ -1,11 +1,10 @@
-module top(input        clk,
-           output [3:0] led,
-           output       r,
-           output       g,
-           output       b,
-           output       pclk,
-           output       de,
-           output       disp_on);
+module top(input         clk,
+           output [3:0]  led,
+           output [15:0] rgb565,
+           output        pclk,
+           output        de,
+           output        disp_on,
+           input [1:0]   btn);
 
 //counter in divider 50Mhz/6-> 8,333Mhz
 reg [6:0] c = 0;
@@ -14,6 +13,10 @@ parameter TDEH = 480;
 parameter TDEL = 256;
 parameter TDEB = 45;  
 parameter TDE = 272;  
+
+reg [4:0] r;
+reg [5:0] g;
+reg [4:0] b;
 
 // Horizontal and vertical counter
 reg [9:0] Hcount;
@@ -43,11 +46,11 @@ SB_PLL40_CORE #(
 always @(posedge clk_50m)
 begin
     if (c < 3) begin
-        c = c+1;
-        clk10_out = 1'b1;	 
-    end else if (c>=3 && c<=4) begin
-        c = c+1;
-        clk10_out = 1'b0;
+        c = c + 1;
+        clk10_out = 1'b1;
+    end else if (c <= 3 && c<= 4) begin
+        clk10_out = 1'b0;	
+        c = c + 1;
     end else begin
         c = 0;
     end
@@ -74,9 +77,12 @@ end
 assign de = ((Hcount < TDEH) && (Vcount < TDE)) ? 1'b1 : 1'b0; 
 assign pclk = clk10_out;
 
-assign r = ((Hcount > 0) && (Hcount <= 160)) ? 1'b1 : 1'b0;
-assign g = ((Hcount > 160) && (Hcount <= 320)) ? 1'b1 : 1'b0;
-assign b = ((Hcount > 320) && (Hcount <= 480)) ? 1'b1 : 1'b0;
+assign r = ((Hcount > 0) && (Hcount <= 160)) ? 5'b11111 : 5'b00000;
+assign g = ((Hcount > 160) && (Hcount <= 320)) ? 6'b111111 : 6'b00000;
+assign b = ((Hcount > 320) && (Hcount <= 480)) ? 5'b11111 : 5'b00000;
+
+assign rgb565 = {r, g, b};
 assign disp_on = 1'b1;
+assign led = {btn, 2'b01};
 
 endmodule
