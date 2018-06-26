41c893e40da372e007e5209cb89289a6f9219d79
Trammell hudson <hudson@trmm.net>
2018-06-26 10:31:04 -0400

    use a struct to map the sdram control registers

--- bridge_lib/sdram.c
+++ bridge_lib/sdram.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,58 +11,69 @@
 
 #include "bw_bridge.h"
 
+typedef struct __attribute__((__packed__))
+{
+	volatile uint16_t reset:1; // offset 0
+	volatile uint16_t busy:1;
+	volatile uint16_t rd_enable:1;
+	volatile uint16_t wr_enable:1;
+	volatile uint16_t rd_busy:1;
+
+	volatile uint32_t addr; // 25 bits, offset 1
+	volatile uint16_t wr_data; // 8 bits, offset 3
+	volatile uint16_t rd_data; // 8 bits, offset 4
+} bw_sdram_t;
+
 int main()
 {
 	struct bridge br;
-	int i, j;
-	void *ptr;
-
-
 	if (bridge_init(&br, BW_BRIDGE_MEM_ADR, BW_BRIDGE_MEM_SIZE) < 0)
 		return 1;
 
-	ptr = br.virt_addr;
+	bw_sdram_t * const sdram = br.virt_addr;
 
 	// clr reset
-	*(uint16_t *)(ptr) |= 1;
+	sdram->reset = 1;
 
-	for (i=0;i<100;i++) {
+	for (int i=0;i<100;i++) {
 		//set sdram address
-		*(uint16_t *)(ptr + 2) = 0x51+i;
-		*(uint16_t *)(ptr + 4) = 0x12;
+		sdram->addr = 0x120051 + i;
 
 		//set sdram data
-		*(uint16_t *)(ptr + 6) = i;
+		sdram->wr_data = i;
 
 		//set wr enable flag
-		*(uint16_t *)(ptr) |= 8;
+		sdram->wr_enable = 1;
 
 		//waiting for clr busy bit
-		while ((*(uint16_t *)(ptr) & 2));
+		while (sdram->busy)
+			;
 
 		// clr we enable bit
-		*(uint16_t *)(ptr) &= ~8;
+		sdram->wr_enable = 0;
 	}
 
-	for (i=0;i<100;i++) {
+	for (int i=0;i<100;i++) {
 		//set sdra, address
-                *(uint16_t *)(ptr + 2) = 0x51+i;
-                *(uint16_t *)(ptr + 4) = 0x12;
+		sdram->addr = 0x120051 + i;
 
 		//set rd enable flag
-		*(uint16_t *)(ptr) |= 4;
+		sdram->rd_enable = 1;
 
 		// waiting for clr busy bit
-                while ((*(uint16_t *)(ptr) & 2));
+                while (sdram->busy)
+			;
 
 		// waiting for clr rd ready bit
-	       	while ((*(uint16_t *)(ptr) & 16));
+		while (sdram->rd_busy)
+			;
 
 		// clr rd enable bit
-		*(uint16_t *)(ptr) &= ~4;
+		sdram->rd_enable = 0;
 
-		printf("data: %x  %x\n", *(uint16_t *)(ptr + 8), i);
+		printf("data[%x]=%x\n", i, sdram->rd_data);
  	}
+
 	bridge_close(&br);
 
 	return 0;
