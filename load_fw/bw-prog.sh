a6daf62ce0420ac51b399847210b982daac76af5
Demo User <debian@arm.localdomain>
2018-10-08 22:51:41 +0000

    use the load_fw/bw-prog found in this repository
     - use a relative path in Makefile.beaglewire to bw-prog.sh
    
     - load_fw/bw-prog.sh: brought over changes for better debugging and user
       friendliness

--- load_fw/bw-prog.sh
+++ load_fw/bw-prog.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
+echo "$PWD"
+cp -av $1 /lib/firmware
 
-cp "$1" /lib/firmware
-insmod "$DIR/fpga-load.ko" path=${1##*/}
-rmmod fpga-load.ko
+# turn on command tracing
+set -o xtrace
+
+# change working directory to directory of this running script
+cd "${0%/*}"
+
+# turn off command tracing
+set +o xtrace
+if [ -f fpga-load.ko ] ; then
+    echo Found fpga-load.ko
+else
+    echo ERROR: Did not find fpga-load.ko - You may need to run
+    echo ""
+    echo cd `pwd`
+    echo make
+    echo ""
+    echo Exiting with ERROR
+    exit 1
+fi
+
+# turn on command tracing
+set -o xtrace
+insmod fpga-load.ko path=${1##*/}
+rmmod  fpga-load.ko
