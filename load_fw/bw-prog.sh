de34b362460666841515f9682d4825597b7aea0c
Demo User <debian@arm.localdomain>
2018-10-09 14:10:05 +0000

    fix issue with spurious error printed if ran from location of script
     - change to use dirname command (properly handles implicit "." working dir)
    
     - add additional comments including a USAGE comment

--- load_fw/bw-prog.sh
+++ load_fw/bw-prog.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# USAGE
+#     sudo sh bw-prog.sh bw-fpga.bin
+
 echo "$PWD"
+
+# copy .bin file to kernel location
 cp -av $1 /lib/firmware
+
+# determine directory of this running script
+DIR=$(dirname $0)
 
 # turn on command tracing
 set -o xtrace
 
 # change working directory to directory of this running script
-cd "${0%/*}"
+cd "${DIR}"
 
 # turn off command tracing
 set +o xtrace
@@ -16,7 +24,7 @@ if [ -f fpga-load.ko ] ; then
 else
     echo ERROR: Did not find fpga-load.ko - You may need to run
     echo ""
-    echo cd `pwd`
+    echo cd $(pwd)
     echo make
     echo ""
     echo Exiting with ERROR
